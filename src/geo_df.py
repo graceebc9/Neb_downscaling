503a09ac14fa72d066860cdf5f7dbde1ee08735b
“grace <gb669@cam.ac.uk>
2025-01-19 09:49:39 +0000

    update with local and hpc paths

--- src/geo_df.py
+++ src/geo_df.py
@@ -14,11 +14,11 @@ def load_geodf(nrows):
     geo_df['longitude'] = geo_df.geometry.centroid.x
     return geo_df
 
-def create_geo_df(df):
+def create_geo_df(df, pc_path='/Volumes/T9/2024_Data_downloads/codepoint_polygons_edina/Download_all_postcodes_2378998'):
     pcs_load = df.postcode.str[0:2].unique().tolist()
     pcs_load = df.postcode.str.extract('([A-Z]+)').iloc[:,0].unique().tolist()
     print('Postcodes to load ' , pcs_load)
-    pc_shp = load_pc_shp(pcs_load)
+    pc_shp = load_pc_shp(pcs_load, pc_path)
     
     geo_df = pc_shp.merge(df, left_on='POSTCODE', right_on='postcode', how='inner')
     geo_df['latitude'] = geo_df.geometry.centroid.y 
--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,14 @@
+import os 
 import pandas as pd 
 import geopandas as gpd 
 
-def load_pc_shp(pcs_to_load):
+def load_pc_shp(pcs_to_load, pc_path='/Volumes/T9/2024_Data_downloads/codepoint_polygons_edina/Download_all_postcodes_2378998'):
     ll = []
     for pc in pcs_to_load:
         if len(pc)==1:
-            path = f'/Volumes/T9/2024_Data_downloads/codepoint_polygons_edina/Download_all_postcodes_2378998/codepoint-poly_5267291/one_letter_pc_code/{pc}/{pc}.shp'
+            path = os.path.join(pc_path, f'codepoint-poly_5267291/one_letter_pc_code/{pc}/{pc}.shp')
         else:
-            path = f'/Volumes/T9/2024_Data_downloads/codepoint_polygons_edina/Download_all_postcodes_2378998/codepoint-poly_5267291/two_letter_pc_code/{pc}.shp'
+            path = os.path.join(pc_path,  f'codepoint-poly_5267291/two_letter_pc_code/{pc}.shp' ) 
         sd = gpd.read_file(path)    
         ll.append(sd) 
     pc_shp = pd.concat(ll)
